7ea614526c7db996931531b40cdeaa8e8cd17244
Rick Kauffman <rick@rickkauffman.com>
2021-11-02 09:18:20 -0700

    init2

--- etc/test.py
+++ etc/test.py
@@ -3,6 +3,7 @@ from sys import path
 import os, sys
 import time
 import hpe3parclient
+import requests
 
 # this is a hack to get the hpe driver module
 # and it's utils module on the search path.
@@ -10,22 +11,34 @@ cmd_folder = os.path.realpath(os.path.abspath("..") )
 if cmd_folder not in sys.path:
      sys.path.insert(0, cmd_folder)
 
-from hpe3parclient import client, exceptions
-from utils import *
+requests.packages.urllib3.disable_warnings()
 
-username = "admin"
-password = "hpe"
+from hpe3parclient import client, exceptions
+# from utils import *
+
+username = "apiaccess"
+password = "siesta3"
 
 testVolName = "API-TEST-VOLUME"
 testSNAPName = testVolName+"SNAP"
 testCPGName = "TEST-CPG-NAME"
 
-cl = client.HPE3ParClient("https://10.10.22.241:8080/api/v1")
+cl = client.HPE3ParClient("https://10.132.0.40:8080/api/v1")
 
-# Create Volume Name
-try:
-    cl.createVolume(testVolName, testCPGName, 1000)
-except exceptions.HTTPUnauthorized as ex:
-    print("You must login")
-except Exception as ex:
-    print(ex)
+cl.login("apiaccess", "siesta3")
+
+#cl.createVolume(testVolName, testCPGName, 2048, "foo")
+
+volumes = cl.getVolumes()
+wsapi_version = cl.getWsApiVersion()
+tasks = cl.getAllTasks()
+
+
+print(volumes['members'][0]['name'])
+print('------------------------------')
+print(wsapi_version)
+print(tasks['members'])
+
+for t in tasks['members']:
+    print(t)
+    print('-------------------------------------------------------')
